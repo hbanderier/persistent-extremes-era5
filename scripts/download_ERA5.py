4f82408aab69b29b69074e54d64754145a1c8c27
hbanderier <hbanderier@giub.unibe.ch>
2024-07-19 12:56:42 +0200

    updates to paper

--- scripts/download_ERA5.py
+++ scripts/download_ERA5.py
@@ -1,82 +1,64 @@
 from pathlib import Path
-from jetstream_hugo.definitions import DATADIR, YEARSPL
+from jetstream_hugo.definitions import DATADIR, YEARS
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import calendar
 import cdsapi
 
+basepath = Path(f"{DATADIR}/ERA5/surf")
 
+
+def retrieve(client, request, year, month):
+    last_day = calendar.monthrange(year, month)[1]
+    last_day = str(last_day).zfill(2)
+    month = str(month).zfill(2)
+    year = str(year).zfill(4)
+    ofile = basepath.joinpath(f'raw/{year}{month}.nc')
+    if Path(ofile).is_file():
+        return
+    request.update({"year": year, "month": month})
+    client.retrieve('reanalysis-era5-single-levels', request, ofile)
+    return f"Retrieved {year}, {month}"
+    
+    
 def main():
-    basepath = Path(f"{DATADIR}/ERA5/plev/")
-    c = cdsapi.Client()
-    for year in range(1959, 1960):
-        for month in range(1, 13):
-            month_str = str(month).zfill(2)
-            path = basepath.joinpath(f"uv{year}{month_str}_allglobe.nc")
-            if path.is_file():
-                continue
-            c.retrieve(
-                "reanalysis-era5-pressure-levels",
-                {
-                    "product_type": "reanalysis",
-                    "variable": [
-                        "u_component_of_wind",
-                        "v_component_of_wind",
-                    ],
-                    "pressure_level": [
-                        "150",
-                        "200",
-                        "250",
-                        "300",
-                        "350",
-                        "500",
-                        "700",
-                        "850",
-                    ],
-                    "year": str(year),
-                    "month": month_str,
-                    "day": [
-                        "01",
-                        "02",
-                        "03",
-                        "04",
-                        "05",
-                        "06",
-                        "07",
-                        "08",
-                        "09",
-                        "10",
-                        "11",
-                        "12",
-                        "13",
-                        "14",
-                        "15",
-                        "16",
-                        "17",
-                        "18",
-                        "19",
-                        "20",
-                        "21",
-                        "22",
-                        "23",
-                        "24",
-                        "25",
-                        "26",
-                        "27",
-                        "28",
-                        "29",
-                        "30",
-                        "31",
-                    ],
-                    "time": [
-                        "00:00",
-                        "06:00",
-                        "12:00",
-                        "18:00",
-                    ],
-                    "grid": "1.0/1.0",
-                    "format": "netcdf",
-                },
-                path,
-            )
-
+    request = {
+        'product_type': 'reanalysis',
+        'variable': [
+            '10m_u_component_of_wind', '10m_v_component_of_wind',
+        ],
+        'year': '1959',
+        'month': '01',
+        'day': [
+            '01', '02', '03',
+            '04', '05', '06',
+            '07', '08', '09',
+            '10', '11', '12',
+            '13', '14', '15',
+            '16', '17', '18',
+            '19', '20', '21',
+            '22', '23', '24',
+            '25', '26', '27',
+            '28', '29', '30',
+            '31',
+        ],
+        'time': [
+            '00:00', '06:00', '12:00',
+            '18:00',
+        ],
+        "grid": "0.5/0.5",
+        'format': 'netcdf',
+    }
+    client = cdsapi.Client()
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        futures = [
+            executor.submit(retrieve, client, request.copy(), year, month) for year in YEARS for month in range(1, 13)
+        ]
+        for f in as_completed(futures):
+            try:
+                print(f.result())
+            except:
+                print("could not retrieve")
+        
 
 if __name__ == "__main__":
     main()
