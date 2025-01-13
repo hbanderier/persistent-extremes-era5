514a49e44563ae4ce4b769d8f715115375469174
hbanderier <hbanderier@giub.unibe.ch>
2025-01-13 10:35:20 +0100

    uv500 + tests, part1

--- scripts/download_era5/download_ERA5.py
+++ scripts/download_era5/download_ERA5.py
@@ -4,7 +4,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 import calendar
 import cdsapi
 
-basepath = Path(f"{DATADIR}/ERA5/surf")
+basepath = Path(f"{DATADIR}/ERA5/plev")
 
 
 def retrieve(client, request, year, month):
@@ -16,7 +16,7 @@ def retrieve(client, request, year, month):
     if Path(ofile).is_file():
         return
     request.update({"year": year, "month": month})
-    client.retrieve("derived-era5-single-levels-daily-statistics", request, ofile)
+    client.retrieve("reanalysis-era5-pressure-levels", request, ofile)
     return f"Retrieved {year}, {month}"
     
     
@@ -24,32 +24,32 @@ def main():
     request = {
         "product_type": "reanalysis",
         "variable": [
-            "2m_temperature",
-            "mean_sea_level_pressure",
-            "sea_surface_temperature",
-            "total_precipitation",
+            "u_component_of_wind",
+            "v_component_of_wind"
         ],
-        'year': '1959',
-        'month': '01',
-        'day': [
-            '01', '02', '03',
-            '04', '05', '06',
-            '07', '08', '09',
-            '10', '11', '12',
-            '13', '14', '15',
-            '16', '17', '18',
-            '19', '20', '21',
-            '22', '23', '24',
-            '25', '26', '27',
-            '28', '29', '30',
-            '31',
+        "year": "1982",
+        "month": "01",
+        "day": [
+            "01", "02", "03",
+            "04", "05", "06",
+            "07", "08", "09",
+            "10", "11", "12",
+            "13", "14", "15",
+            "16", "17", "18",
+            "19", "20", "21",
+            "22", "23", "24",
+            "25", "26", "27",
+            "28", "29", "30",
+            "31"
         ],
-        "daily_statistic": "daily_mean",
-        "time_zone": "utc+00:00",
-        "frequency": "6_hourly",
-        "grid": "0.5/0.5",
-        'format': 'netcdf',
-        "area": [90, -180, 0, 180],
+        "time": [
+            "00:00", "06:00", "12:00",
+            "18:00"
+        ],
+        "pressure_level": ["500"],
+        "data_format": "netcdf",
+        "download_format": "unarchived",
+        "area": [90, -180, 0, 180]
     }
     client = cdsapi.Client()
     with ThreadPoolExecutor(max_workers=10) as executor:
