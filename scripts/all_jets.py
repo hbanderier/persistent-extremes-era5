29f30aa594d313d4032c04d77f9cef702848c165
hbanderier <hbanderier@giub.unibe.ch>
2025-01-22 15:32:35 +0100

    after paper revs + some pers

--- scripts/all_jets.py
+++ scripts/all_jets.py
@@ -1,11 +1,11 @@
+from jetstream_hugo.definitions import DATADIR
 from jetstream_hugo.jet_finding import JetFindingExperiment
 from jetstream_hugo.data import DataHandler
 import xarray as xr
 
 if __name__ == '__main__':
-    dh_high = DataHandler.from_specs("ERA5", "plev", "high_wind", "6H", "all", None, -80, 40, 15, 80, "all")
-    dh_low = DataHandler.from_specs("ERA5", "plev", "low_wind", "6H", "all", None, -80, 40, 15, 80, "all")
-    exp = JetFindingExperiment(dh_high)
+    exp = JetFindingExperiment(DataHandler(f"{DATADIR}/ERA5/plev/high_wind/6H/results/6"))
+    dh_low = DataHandler.from_specs("ERA5", "plev", "mid_wind", "6H", "all", None, -80, 40, 15, 80, "all")
     exp.find_jets()
     exp.categorize_jets(dh_low.da["s"])
     exp.track_jets()
