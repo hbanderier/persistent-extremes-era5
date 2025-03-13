9e41dcf0dea8f485287a433bca5491bffa8ae551
hbanderier <hbanderier@giub.unibe.ch>
2025-03-13 13:28:42 +0100

    three things in parallel

--- scripts/download_era5/download_arco_ERA5.py
+++ scripts/download_era5/download_arco_ERA5.py
@@ -1,11 +1,23 @@
 from pathlib import Path
 import numpy as np
-from tqdm import trange
-from jetstream_hugo.data import extract, flatten_by, standardize
-from jetstream_hugo.definitions import DATADIR, compute
+import xarray as xr
+from jetstream_hugo.data import standardize
+from jetstream_hugo.definitions import DATADIR, compute, YEARS
 import argparse
 
-import xarray as xr
+parser = argparse.ArgumentParser(description="sample argument parser")
+parser.add_argument("year")
+parser.add_argument("varname")
+args = parser.parse_args()
+year = int(args.year)
+varname = args.varname
+
+var_map = {
+    "t2m": ["2m_temperature", np.mean],
+    "tp": ["total_precipitation", np.sum],
+}
+long_name = var_map[varname][0]
+func = var_map[varname][1]
 
 ds = xr.open_zarr(
     "gs://gcp-public-data-arco-era5/ar/full_37-1h-0p25deg-chunk-1.zarr-v3",
@@ -16,36 +28,29 @@ ar_full_37_1h = ds.sel(
     time=slice(ds.attrs["valid_time_start"], ds.attrs["valid_time_stop"])
 )
 
+base_ds = standardize(ar_full_37_1h[long_name].chunk("auto"))
 base_ds = (
-    ar_full_37_1h[["u_component_of_wind", "v_component_of_wind"]]
+    base_ds
     .sel(
-        time=ar_full_37_1h.time.dt.hour % 6 == 0,
-        latitude=ar_full_37_1h.latitude >= 0,
-        level=[175, 200, 225, 250, 300, 350],
+        lat=base_ds.lat >= 0,
+        time=np.isin(base_ds.time.dt.year, YEARS)
     )
-    .isel(longitude=slice(None, None, 2), latitude=slice(None, None, 2))
+    .isel(lon=slice(None, None, 2), lat=slice(None, None, 2))
 )
 
-parser = argparse.ArgumentParser(description="sample argument parser")
-parser.add_argument("year")
-args = parser.parse_args()
-year = int(args.year)
+base_path_1 = Path(f"{DATADIR}/ERA5/surf/{varname}/6H")
+base_path_1.mkdir(exist_ok=True, parents=True)
+base_path_2 = Path(f"{DATADIR}/ERA5/surf/{varname}/daily{func.__name__}")
+base_path_2.mkdir(exist_ok=True, parents=True)
 
-base_path = Path(f"{DATADIR}/ERA5/plev/flat_wind/dailymean")
-for month in trange(1, 13):
-    month_str = str(month).zfill(2)
-    opath = base_path.joinpath(f"{year}{month_str}.nc")
-    if opath.is_file():
-        print(f"Already had {year}{month}")
-        continue
-    ds = _compute(
-        base_ds.sel(
-            time=(base_ds.time.dt.year == year) & (base_ds.time.dt.month == month)
-        ),
-        progress=True,
-    )
-    ds = standardize(ds)
-    ds["s"] = np.sqrt(ds["u"] ** 2 + ds["v"] ** 2)
-    ds = flatten_by(ds, "s")
-    ds.to_netcdf(opath)
-    print(f"Completed {year}{month}")
+six_hourly = base_ds.sel(time=base_ds.time.dt.year == year).resample(time="6h").reduce(func)
+opath_1 = base_path_1.joinpath(f"{year}.nc")
+opath_2 = base_path_2.joinpath(f"{year}.nc")
+if not opath_1.is_file() or not opath_2.is_file():
+    six_hourly = compute(six_hourly, progress_flag=True)
+if not opath_1.is_file():
+    six_hourly.to_netcdf(opath_1)
+if not opath_2.is_file():
+    daily = six_hourly.resample(time="1d").reduce(func)
+    daily.to_netcdf(opath_2)
+print(f"Completed {year}")
