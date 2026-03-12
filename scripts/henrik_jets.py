df23bfbf99d9b210adb6f8b8013112f856245f34
hbanderier <hbanderier@giub.unibe.ch>
2026-03-12 22:06:03 +0100

    gg

--- scripts/henrik_jets.py
+++ scripts/henrik_jets.py
@@ -1,17 +1,21 @@
 import os
 from pathlib import Path
 
-import dask.array as da
-import geopandas as gpd
+import dask.array as darr
 import numpy as np
-import pandas as pd
 import polars as pl
-import wavebreaking as wb
 import xarray as xr
-from jetutils.data import open_da
-from jetutils.definitions import DATADIR, YEARS, compute
+from jetutils.data import open_da, smooth
+from jetutils.definitions import DATADIR, YEARS, compute, N_WORKERS
 from jetutils.derived_quantities import compute_absolute_vorticity
-from jetutils.geospatial import gather_normal_da_jets, interp_jets_to_zero_one
+from jetutils.geospatial import (
+    gather_normal_da_jets,
+    interp_jets_to_zero_one,
+    compute_contours,
+    compute_overturnings,
+    event_props,
+    to_xarray_sjoin,
+)
 from jetutils.jet_finding import (
     DataHandler,
     JetFindingExperiment,
@@ -19,8 +23,7 @@ from jetutils.jet_finding import (
     iterate_over_year_maybe_member,
 )
 from scipy.signal.windows import lanczos
-from tqdm import tqdm
-from wavebreaking import to_xarray
+from tqdm import tqdm, trange
 
 os.environ["RUST_BACKTRACE"] = "full"
 
@@ -29,7 +32,7 @@ def convolve(in1, in2, mode="full", method="fft", axes=None):
     from scipy.signal import fftconvolve, oaconvolve
     from scipy.signal._signaltools import _init_freq_conv_axes
 
-    in1 = da.asarray(in1)
+    in1 = darr.asarray(in1)
     in2 = np.asarray(in2)
 
     # Checking for trivial cases and incorrect flags
@@ -96,7 +99,7 @@ def convolve(in1, in2, mode="full", method="fft", axes=None):
             )
             in1 = in1.reshape(new_shape)
             for a in not_axes_but_s1_1:
-                in1 = da.stack([in1] * s2[a], axis=a)
+                in1 = darr.stack([in1] * s2[a], axis=a)
             return convolve(in1, in2, mode=mode, method=method, axes=axes)
 
         # Deals with the case where there is at least one axis a in which we do not
@@ -125,14 +128,14 @@ def convolve(in1, in2, mode="full", method="fft", axes=None):
             pad_width = tuple(
                 (even_flag[a], 0) if a in axes else (0, 0) for a in range(in1.ndim)
             )
-            in2 = da.pad(in2, pad_width)
+            in2 = darr.pad(in2, pad_width)
 
         if mode != "valid":
             pad_width = tuple(
                 (depth[i] - even_flag[i], depth[i]) if i in axes else (0, 0)
                 for i in range(in1.ndim)
             )
-            in1 = da.pad(in1, pad_width)
+            in1 = darr.pad(in1, pad_width)
 
         if mode == "periodic":
             boundary = "periodic"
@@ -140,7 +143,7 @@ def convolve(in1, in2, mode="full", method="fft", axes=None):
             boundary = 0
 
         cv_dict = {"oa": oaconvolve, "fft": fftconvolve}
-        
+
         def cv_func(x):
             return cv_dict[method](x, in2, mode="same", axes=axes)
 
@@ -226,11 +229,10 @@ for run in ["ctrl", "dobl"]:
             print("zeta", oname, run)
 
 # block 2: compute EMF
-
 for run in ["ctrl", "dobl"]:
     opath = Path(
         f"{DATADIR}/Henrik_data/{run}/high_wind/6H/results",
-        "Eddy_uv300_natl_10days.zarr",
+        "Eddy_uv300_NH_10days.zarr",
     )
     if opath.is_dir():
         continue
@@ -241,7 +243,7 @@ for run in ["ctrl", "dobl"]:
             combine="nested",
             concat_dim="time",
         )[["u", "v"]]
-        .sel(lon=slice(-80, 40), lat=slice(15, 80), lev=30000)
+        .sel(lat=slice(0, 90), lev=30000)
         .chunk("auto")
     )
     l_win = lanczos(2 * half_len + 1)[:, None, None]
@@ -271,86 +273,158 @@ for run in ["ctrl", "dobl"]:
     compute(res, progress=True)
 
 
-# stage 3: compute wavebreaking
+levels = (
+    compute(
+        xr.open_mfdataset(
+            list(Path(DATADIR, "Henrik_data/ctrl/zeta/6H").glob("*0.nc"))
+        )["__xarray_dataarray_variable__"].quantile([0.25, 0.375, 0.5, 0.625, 0.75]),
+        progress_flag=True,
+    ).values
+    * 1e5
+).tolist()
 for run in ["ctrl", "dobl"]:
     basepath_zeta = Path(DATADIR, f"Henrik_data/{run}/zeta/6H")
     da_mflux = xr.open_dataset(
-        f"{DATADIR}/Henrik_data/{run}/high_wind/6H/results/Eddy_uv300_natl_10days.zarr"
+        f"{DATADIR}/Henrik_data/{run}/high_wind/6H/results/Eddy_uv300_NH_10days.zarr"
     )["EMF"]
-    opath = Path(DATADIR, f"Henrik_data/{run}/rwb_index")
-    opath.mkdir(exist_ok=True)
-    for year in range(1969, 2021):
-        zeta = xr.open_dataarray(basepath_zeta.joinpath(f"{year}.nc")).sel(lev=30000)
-        zeta = zeta.assign_coords(
-            lat=0.7 + np.arange(len(zeta.lat), dtype=np.float128) * 0.94
-        ).rename("zeta")
-        mflux = da_mflux.sel(time=zeta.time)
-        mflux = mflux.assign_coords(
-            lat=0.7 + np.arange(len(mflux.lat), dtype=np.float128) * 0.94
-        ).rename("mflux")
-        ofile = opath.joinpath(f"overturnings_{year}.parquet")
+    opath = Path(DATADIR, "Henrik_data", run)
+    opath_rwb = Path(DATADIR, f"Henrik_data/{run}/rwb_index")
+    opath_rwb.mkdir(exist_ok=True)
+    for year in trange(1969, 2021):
+        ofile = opath_rwb.joinpath(f"overturnings_{year}.parquet")
         if ofile.is_file():
-            continue
-        zeta_ = wb.calculate_smoothed_field(
-            data=zeta,
-            passes=5,
-            weights=np.array([[0, 1, 0], [1, 2, 1], [0, 1, 0]]),
-            mode="wrap",
-        )
-        out = wb.calculate_overturnings(
-            data=zeta_ * 1e5,
-            contour_levels=[7, 8, 9, 10],
-            intensity=mflux,  # optional
-            periodic_add=120,
-        )
-        out[0].to_parquet(ofile)
-        
-# stage 4: rwb das
-varname = "ones"
-dtype = {"ones": np.uint32, "intensity": np.float32, "mean_var": np.float32, "event_area": np.float32}[varname]
-coords = {
-    "time": pd.date_range("19690101", "20210101", freq="6h", inclusive="left"),
-    "lat": np.arange(0, 90.5, 1),
-    "lon": np.arange(-100, 60.5, 1),
-}
-shape = [len(co) for co in coords.values()]
-dummy_da = xr.DataArray(np.zeros(shape, dtype=dtype), coords=coords)
+            overturnings = pl.read_parquet(ofile)
+            overturnings_on_grid = None
+        else:
+            zeta = xr.open_dataarray(basepath_zeta.joinpath(f"{year}.nc")).sel(
+                lev=30000
+            )
+            zeta = (
+                zeta.assign_coords(
+                    lat=(0.7 + np.arange(len(zeta.lat), dtype=np.float32) * 0.94).round(
+                        2
+                    )
+                )
+                .rename("zeta")
+                .reset_coords(["lev", "loni", "lati"], drop=True)
+                * 1e5
+            )
+            mflux = da_mflux.sel(time=zeta.time)
+            mflux = (
+                mflux.assign_coords(
+                    lat=(
+                        0.7 + np.arange(len(mflux.lat), dtype=np.float32) * 0.94
+                    ).round(2)
+                )
+                .rename("mflux")
+                .reset_coords("lev", drop=True)
+            )
+            zeta = smooth(zeta, {"lon": ("win", 3), "lat": ("win", 3)})
+            zeta = compute(zeta)
+            mflux = smooth(mflux, {"lon": ("win", 3), "lat": ("win", 3)})
+            mflux = compute(mflux)
+            contours = compute_contours(zeta, levels, processes=N_WORKERS, ctx="fork")
+            overturnings = compute_overturnings(contours)
+            overturnings, overturnings_on_grid = event_props(
+                overturnings, [zeta, mflux]
+            )
+            overturnings.write_parquet(ofile)
 
-if not Path(DATADIR, "Henrik_data", "dobl", "APVO/dailyany/full.nc").is_file():
-    for run in ["ctrl", "dobl"]:
-        all_events = {}
-        basepath = Path(DATADIR, "Henrik_data", run, "rwb_index")
-        files = list(sorted(basepath.glob("overturnings_????.parquet")))
-        events = []
-        for f in files:
-            events.append(gpd.read_parquet(f))
-        events = pd.concat(events)
-        all_events["APVO"] = events[events.orientation == "anticyclonic"]
-        all_events["CPVO"] = events[events.orientation == "cyclonic"]
-        opath = Path(DATADIR, "Henrik_data", run)
-        for name in ["APVO", "CPVO"]:
-            da_rwb = to_xarray(dummy_da, all_events[name], flag=varname).rename(name)
-            opath.joinpath(f"{name}/6H").mkdir(parents=True, exist_ok=True)
-            da_rwb.to_netcdf(opath.joinpath(f"{name}/6H/full.nc"))
-            da_rwb = da_rwb.resample(time="1D").any().astype(np.uint8)
-            opath.joinpath(f"{name}/dailyany").mkdir(parents=True, exist_ok=True)
-            da_rwb.to_netcdf(opath.joinpath(f"{name}/dailyany/full.nc"))
+        for orientation in ["cyclonic", "anticyclonic"]:
+            name = f"{orientation[0].upper()}AVO"
+            odir = opath.joinpath(f"{name}/6H")
+            odir.mkdir(parents=True, exist_ok=True)
+            ofile = odir.joinpath(f"{year}.nc")
+            if ofile.is_file():
+                continue
+            df = overturnings.filter(pl.col("orientation") == orientation)
+            da = to_xarray_sjoin(
+                zeta, events=overturnings, events_on_grid=overturnings_on_grid
+            )
+            da.to_netcdf(ofile)
+
+            odir = opath.joinpath(f"{name}/dailyany")
+            odir.mkdir(parents=True, exist_ok=True)
+            da = da.any("level").resample(time="1D").any().astype(np.uint8)
+            da.to_netcdf(odir.joinpath(f"{year}.nc"))
+
 
 # stage 5: define jets (already computed probably)
 both_jets = {}
 both_paths = {}
 for run in ["ctrl", "dobl"]:
-    dh = DataHandler.from_specs("Henrik_data", run, ("high_wind", ["u", "v", "s", "theta"]), "6H", minlon=-80, maxlon=40, minlat=15, maxlat=80, levels=20000)
+    dh = DataHandler.from_specs(
+        "Henrik_data",
+        run,
+        ("high_wind", ["u", "v", "s", "theta"]),
+        "6H",
+        minlon=-80,
+        maxlon=40,
+        minlat=15,
+        maxlat=80,
+        levels=20000,
+    )
     exp = JetFindingExperiment(dh)
-    all_jets_one_df = exp.find_jets(force=False, n_coarsen=1, smooth_s=5, alignment_thresh=0.55, base_s_thresh=0.55, int_thresh_factor=0.2, hole_size=5)
-    theta300 = open_da("Henrik_data", run, ("high_wind", ["s", "theta"]), "6H", minlon=-80, maxlon=40, minlat=15, maxlat=80, levels=30000).rename({"s": "s300", "theta": "theta300"})
-    all_jets_one_df = add_feature_for_cat(all_jets_one_df, "s300", theta300, ofile_ajdf=exp.path.joinpath("all_jets_one_df.parquet"), force=False)
-    all_jets_one_df = add_feature_for_cat(all_jets_one_df, "theta300", theta300, ofile_ajdf=exp.path.joinpath("all_jets_one_df.parquet"), force=False)
-    all_jets_one_df = exp.categorize_jets(None, ["s300", "theta300"], force=False, n_init=15, init_params="k-means++", mode="month").cast({"time": pl.Datetime("ms")})
-    phat_filter = (pl.col("is_polar") < 0.5) | ((pl.col("is_polar") > 0.5) & (pl.col("int") > 1.e8))
-    phat_jets = all_jets_one_df.filter((pl.col("is_polar").mean().over(["time", "jet ID"]) < 0.5) | ((pl.col("is_polar").mean().over(["time", "jet ID"]) > 0.5) & (pl.col("int").mode().first().over(["time", "jet ID"]) > 1.e8)))
+    all_jets_one_df = exp.find_jets(
+        force=False,
+        n_coarsen=1,
+        smooth_s=5,
+        alignment_thresh=0.55,
+        base_s_thresh=0.55,
+        int_thresh_factor=0.2,
+        hole_size=5,
+    )
+    theta300 = open_da(
+        "Henrik_data",
+        run,
+        ("high_wind", ["s", "theta"]),
+        "6H",
+        minlon=-80,
+        maxlon=40,
+        minlat=15,
+        maxlat=80,
+        levels=30000,
+    ).rename({"s": "s300", "theta": "theta300"})
+    all_jets_one_df = add_feature_for_cat(
+        all_jets_one_df,
+        "s300",
+        theta300,
+        ofile_ajdf=exp.path.joinpath("all_jets_one_df.parquet"),
+        force=False,
+    )
+    all_jets_one_df = add_feature_for_cat(
+        all_jets_one_df,
+        "theta300",
+        theta300,
+        ofile_ajdf=exp.path.joinpath("all_jets_one_df.parquet"),
+        force=False,
+    )
+    all_jets_one_df = exp.categorize_jets(
+        None,
+        ["s300", "theta300"],
+        force=False,
+        n_init=15,
+        init_params="k-means++",
+        mode="month",
+    ).cast({"time": pl.Datetime("ms")})
+    phat_filter = (pl.col("is_polar") < 0.5) | (
+        (pl.col("is_polar") > 0.5) & (pl.col("int") > 1.0e8)
+    )
+    phat_jets = all_jets_one_df.filter(
+        (pl.col("is_polar").mean().over(["time", "jet ID"]) < 0.5)
+        | (
+            (pl.col("is_polar").mean().over(["time", "jet ID"]) > 0.5)
+            & (pl.col("int").mode().first().over(["time", "jet ID"]) > 1.0e8)
+        )
+    )
     both_paths[run] = exp.path
-    both_jets[run] = phat_jets.with_columns(**{"jet ID": (pl.col("is_polar").mean().over(["time", "jet ID"]) > 0.5).cast(pl.UInt32())})
+    both_jets[run] = phat_jets.with_columns(
+        **{
+            "jet ID": (pl.col("is_polar").mean().over(["time", "jet ID"]) > 0.5).cast(
+                pl.UInt32()
+            )
+        }
+    )
 
 # stage 6: Interpolate new fields
 args = ["all", None, -100, 60, 0, 90]
@@ -365,17 +439,19 @@ to_do = (
     ("t_low", "t_low", {"levels": 100000}),
     ("PTTEND500", ("heating", "PTTEND"), {"levels": 50000}),
     ("DTCOND500", ("heating", "DTCOND"), {"levels": 50000}),
-    ("APVO", "APVO", {}),
-    ("CPVO", "CPVO", {}),
+    ("AAVO", "AAVO", {}),
+    ("CAVO", "CAVO", {}),
 )
 
 for run in ["ctrl", "dobl"]:
     for huh in to_do:
-        ofile = both_paths[run].joinpath(f"{da.name}_meters_relative.parquet")
+        rename, name, kwargs = huh
+        ofile = both_paths[run].joinpath(f"{rename}_relative.parquet")
         if ofile.is_file():
             continue
-        rename, name, kwargs = huh
         da_ = open_da("Henrik_data", run, name, "6H", *args, **kwargs).rename(rename)
+        if rename in ["AAVO", "CAVO"]:
+            da_ = da_.isel(lev=2)
         da_ = compute(da_)
         interpd = create_jet_relative_dataset(both_jets[run], da_)
         del da_
